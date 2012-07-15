eaf85f8052f027149ffe7e7512b138033adefac9
Andree Brazeau <andreebrazeau@gmail.com>
2012-07-14 19:27:57 -0700

    7:27

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // max_height = $('#container').height();
+  nb_rows = 3
+
+  $('img').height(250);
+  for (var i = Things.length - 1; i >= 0; i--) {
+  	Things[i]
+  };
+});
